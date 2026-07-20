32851fa90f68f4b02f9aefeb0ca59327a9eb9d64
BANG-R <raiqudrat17@gmail.com>
2026-07-20 12:50:29 +0700

    fix: add trustProxies in bootstrap/app.php for HTTPS asset rendering

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Percayai semua Reverse Proxy (Railway) agar asset ter-render via HTTPS
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
